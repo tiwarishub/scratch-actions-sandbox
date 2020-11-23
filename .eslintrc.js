afc5dbff81ed82ff6f53143924f9178a9736e789
Austin McGee <947888+amcgee@users.noreply.github.com>
2020-11-23 22:37:25 +0100

    chore: add missing config files

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+const { config } = require('@dhis2/cli-style')
+
+module.exports = {
+    extends: [config.eslintReact],
+    globals: {
+        Cypress: "readonly",
+        after: "readonly",
+        before: "readonly",
+        cy: "readonly",
+        assert: "readonly",
+    },
+}
